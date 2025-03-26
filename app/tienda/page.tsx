80e5072ab442f3e5636b82329df1464fc5bf0296
Joa018 <xgettcervio@gmail.com>
2025-03-26 01:33:08 -0300

    hooks routes anidadas

--- app/tienda/page.tsx
+++ app/tienda/page.tsx
@@ -1,10 +1,15 @@
+import Image from "next/image";
 import Link from "next/link";
 import { ReactNode } from "react";
 
-function Tienda():React.ReactNode{
-    return(
+function Tienda(): React.ReactNode {
+
+
+
+    return (
         <div>
-        <h1>Tienda</h1>
+            <h1>Tienda</h1>
+            <Image src="../public/next.svg" width={1000} height={500} className="hidden md:block" alt="imagen de NEXTJS" />
         </div>
     )
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import {Montserrat, Roboto} from 'next/font/google';
+
+
+export const montserrat = Montserrat<undefined>({subsets:['latin']})
+
+export const roboto = Roboto<undefined>({subsets:['latin']})
+
+
+//CODE SPLITING
